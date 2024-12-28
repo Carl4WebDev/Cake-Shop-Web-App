80364c99631a99161160ee26ffb12993b65add4d
carl Ivan Sampan <carlforwebdev@gmail.com>
2024-12-28 11:05:26 +0600

    25. StoreController- Add pagination / Learned: pagination tricks

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -1,4 +1,5 @@
-﻿using BakeryStoreMVC.Services;
+﻿using BakeryStoreMVC.Models;
+using BakeryStoreMVC.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BakeryStoreMVC.Controllers
@@ -6,16 +7,29 @@ namespace BakeryStoreMVC.Controllers
     public class StoreController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly int pageSize = 8;
         public StoreController(ApplicationDbContext context) {
             this.context = context;
-        }
-        public IActionResult Index()
+        } 
+        public IActionResult Index(int pageIndex)
         {
-            var products = context.Product.OrderByDescending(p => p.Id).ToList();
-            
+            IQueryable<Product> query = context.Product;
 
+            query = query.OrderByDescending(p=> p.Id);
+
+            if (pageIndex < 1) {
+                pageIndex = 1;
+            }
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count/ pageSize);
+            query = query.Skip((pageIndex -1) * pageSize).Take(pageSize);
+
+
+            var products = query.ToList(); 
             ViewBag.Products = products;
-
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
             return View();
         }
     }
--- obj/Debug/net8.0/BakeryStoreMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/BakeryStoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakeryStoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c04bddd132e0e504c0ef1d387fd93aef27216072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ee2889ebf91af06497ed4510b917d55614b23c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakeryStoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BakeryStoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
