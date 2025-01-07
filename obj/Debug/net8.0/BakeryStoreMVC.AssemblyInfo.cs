dbaa7c2317e5a32751c6a15ec17cc993924a0b6d
carl Ivan Sampan <carlforwebdev@gmail.com>
2025-01-07 20:23:42 +0600

    57. Read the cookie on the server and display the cart size

--- obj/Debug/net8.0/BakeryStoreMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/BakeryStoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakeryStoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e4ff3c23824a511993624af403bd335e8b3e8f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b94bd9aeb657ee376299221ddb3b04773227251a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakeryStoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BakeryStoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
