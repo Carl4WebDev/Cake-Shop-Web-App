d58a5881c6d6b6005f65e50546d099582fdf9c97
carl Ivan Sampan <carlforwebdev@gmail.com>
2025-01-08 23:20:41 +0600

    62-63. Display orders to admin

--- obj/Debug/net8.0/BakeryStoreMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/BakeryStoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakeryStoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8fbe43843db6eb66f9bc73dfcfff881d48ce01b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c1d9badc1d048de93b8682f305138b3b82bbba")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakeryStoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BakeryStoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
